cf8ee5084e305f729fda4520e4a6e674df8604d5
lanly_dev <lanly-dev@users.noreply.github.com>
2024-06-11 09:58:38 -0700

    wip: lab - langchain 4 - done

--- labs/langchain/index.js
+++ labs/langchain/index.js
@@ -12,6 +12,14 @@ const { PromptTemplate } = require('@langchain/core/prompts')
 const { RunnableSequence, RunnablePassthrough } = require('@langchain/core/runnables')
 const { StringOutputParser } = require('@langchain/core/output_parsers')
 
+// For LangChain agent
+const { DynamicTool } = require('@langchain/core/tools')
+const { AgentExecutor } = require('langchain/agents')
+const { MessagesPlaceholder, ChatPromptTemplate } = require('@langchain/core/prompts')
+const { convertToOpenAIFunction } = require('@langchain/core/utils/function_calling')
+const { OpenAIFunctionsAgentOutputParser } = require('langchain/agents/openai/output_parser')
+const { formatToOpenAIFunctionMessages } = require('langchain/agents/format_scratchpad')
+
 // set up the MongoDB client
 const dbClient = new MongoClient(process.env.AZURE_COSMOSDB_CONNECTION_STRING)
 
@@ -43,8 +51,12 @@ async function main() {
     // )
     // console.log(results)
 
-    // 'Requests to the ChatCompletions_Create Operation under Azure OpenAI API version 2023-09-01-preview have exceeded token rate limit of your current OpenAI S0 pricing tier. Please retry after 86400 seconds. Please go here: https://aka.ms/oai/quotaincrease if you would like to further increase the default rate limit.'
-    console.log(await ragLCELChain('What yellow products do you have?'))
+    // // RAG with LangChain
+    // console.log(await ragLCELChain('What yellow products do you have?'))
+
+    // LangChain agent
+    const agentExecutor = await buildAgentExecutor()
+    console.log(await executeAgent(agentExecutor, 'What yellow products do you have?'))
   } catch (err) {
     console.error(err)
   } finally {
@@ -53,6 +65,7 @@ async function main() {
   }
 }
 
+// RAG with LangChain ⬇⬇⬇
 function formatDocuments(docs) {
   // Prepares the product list for the system prompt.
   let strDocs = ''
@@ -86,25 +99,25 @@ async function ragLCELChain(question) {
   // A system prompt describes the responsibilities, instructions, and persona of the AI.
   // Note the addition of the templated variable/placeholder for the list of products and the incoming question.
   const systemPrompt = `
-      You are a helpful, fun and friendly sales assistant for Cosmic Works, a bicycle and bicycle accessories store. 
-      Your name is Cosmo.
-      You are designed to answer questions about the products that Cosmic Works sells.
+    You are a helpful, fun and friendly sales assistant for Cosmic Works, a bicycle and bicycle accessories store. 
+    Your name is Cosmo.
+    You are designed to answer questions about the products that Cosmic Works sells.
 
-      Only answer questions related to the information provided in the list of products below that are represented
-      in JSON format.
+    Only answer questions related to the information provided in the list of products below that are represented
+    in JSON format.
 
-      If you are asked a question that is not in the list, respond with "I don't know."
+    If you are asked a question that is not in the list, respond with "I don't know."
 
-      Only answer questions related to Cosmic Works products, customers, and sales orders.
+    Only answer questions related to Cosmic Works products, customers, and sales orders.
 
-      If a question is not related to Cosmic Works products, customers, or sales orders,
-      respond with "I only answer questions about Cosmic Works"
+    If a question is not related to Cosmic Works products, customers, or sales orders,
+    respond with "I only answer questions about Cosmic Works"
 
-      List of products:
-      {products}
+    List of products:
+    {products}
 
-      Question:
-      {question}
+    Question:
+    {question}
   `
 
   // Use a retriever on the Cosmos DB vector store
@@ -126,8 +139,116 @@ async function ragLCELChain(question) {
     chatModel,
     new StringOutputParser()
   ])
-
   return await ragChain.invoke(question)
 }
+// RAG with LangChain ⬆⬆⬆
+
+// LangChain agent ⬇⬇⬇
+async function buildAgentExecutor() {
+  // A system prompt describes the responsibilities, instructions, and persona of the AI.
+  // Note the variable placeholders for the list of products and the incoming question are not included.
+  // An agent system prompt contains only the persona and instructions for the AI.
+  const systemMessage = `
+    You are a helpful, fun and friendly sales assistant for Cosmic Works, a bicycle and bicycle accessories store.
+
+    Your name is Cosmo.
+
+    You are designed to answer questions about the products that Cosmic Works sells, the customers that buy them, and the sales orders that are placed by customers.
+
+    If you don't know the answer to a question, respond with "I don't know."
+
+    Only answer questions related to Cosmic Works products, customers, and sales orders.
+
+    If a question is not related to Cosmic Works products, customers, or sales orders,
+    respond with "I only answer questions about Cosmic Works"
+  `
+
+  // Create vector store retriever chain to retrieve documents and formats them as a string for the prompt.
+  const retrieverChain = vectorStore.asRetriever().pipe(formatDocuments)
+
+  // Define tools for the agent can use, the description is important this is what the AI will
+  // use to decide which tool to use.
+
+  // A tool that retrieves product information from Cosmic Works based on the user's question.
+  const productsRetrieverTool = new DynamicTool({
+    name: 'products_retriever_tool',
+    description: `Searches Cosmic Works product information for similar products based on the question. 
+                  Returns the product information in JSON format.`,
+    func: async (input) => await retrieverChain.invoke(input)
+  })
+
+  // A tool that will lookup a product by its SKU. Note that this is not a vector store lookup.
+  const productLookupTool = new DynamicTool({
+    name: 'product_sku_lookup_tool',
+    description: `Searches Cosmic Works product information for a single product by its SKU.
+                  Returns the product information in JSON format.
+                  If the product is not found, returns null.`,
+    func: async (input) => {
+      const db = dbClient.db('cosmic_works')
+      const products = db.collection('products')
+      const doc = await products.findOne({ sku: input })
+      if (doc) {
+        //remove the contentVector property to save on tokens
+        delete doc.contentVector
+      }
+      return doc ? JSON.stringify(doc, null, '\t') : null
+    }
+  })
+
+  // Generate OpenAI function metadata to provide to the LLM
+  // The LLM will use this metadata to decide which tool to use based on the description.
+  const tools = [productsRetrieverTool, productLookupTool]
+  const modelWithFunctions = chatModel.bind({
+    functions: tools.map((tool) => convertToOpenAIFunction(tool))
+  })
+
+  // OpenAI function calling is fine-tuned for tool using therefore you don't need to provide instruction.
+  // All that is required is that there be two variables: `input` and `agent_scratchpad`.
+  // Input represents the user prompt and agent_scratchpad acts as a log of tool invocations and outputs.
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', systemMessage],
+    ['human', '{input}'],
+    new MessagesPlaceholder((variable_name = 'agent_scratchpad'))
+  ])
+
+  // Define the agent and executor
+  // An agent is a type of chain that reasons over the input prompt and has the ability
+  // to decide which function(s) (tools) to use and parses the output of the functions.
+  const runnableAgent = RunnableSequence.from([
+    {
+      input: (i) => i.input,
+      agent_scratchpad: (i) => formatToOpenAIFunctionMessages(i.steps)
+    },
+    prompt,
+    modelWithFunctions,
+    new OpenAIFunctionsAgentOutputParser()
+  ])
+
+  // An agent executor can be thought of as a runtime, it orchestrates the actions of the agent
+  // until completed. This can be the result of a single or multiple actions (one can feed into the next).
+  // Note: If you wish to see verbose output of the tool usage of the agent,
+  //       set returnIntermediateSteps to true
+  const executor = AgentExecutor.fromAgentAndTools({
+    agent: runnableAgent,
+    tools
+    //returnIntermediateSteps: true
+  })
+
+  return executor
+}
+
+// Helper function that executes the agent with user input and returns the string output
+async function executeAgent(agentExecutor, input) {
+  // Invoke the agent with the user input
+  const result = await agentExecutor.invoke({ input })
+
+  // Output the intermediate steps of the agent if returnIntermediateSteps is set to true
+  if (agentExecutor.returnIntermediateSteps) {
+    console.log(JSON.stringify(result.intermediateSteps, null, 2))
+  }
+  // Return the final response from the agent
+  return result.output
+}
+// LangChain agent ⬆⬆⬆
 
 main().catch(console.error)
